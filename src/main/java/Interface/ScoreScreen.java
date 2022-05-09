7c6a19c62a828137670ab02879694eea2d4ced79
PTH <hyider2378@gmail.com>
2022-05-09 23:13:38 +0900

    수틀리면복구할거

--- src/main/java/Interface/ScoreScreen.java
+++ src/main/java/Interface/ScoreScreen.java
@@ -8,29 +8,26 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class ScoreScreen extends JFrame {
-    public static int hitCount;
-    public static int missCount;
-    public static int totalCount;
+    public static int hitCount; // 타겟을 클릭한 횟수 변수
+    public static int missCount; // 타겟 외의 곳을 클릭한 횟수 변수
+    public static int totalCount; // 타겟이 나타난 전체 횟수 변수
 
-    public static double percent;
-    public static String percentToString;
-    public static String percentResult;
+    public static double percent; // 타겟을 맞춘 퍼센트 변수
+    public static String percentToString; // 퍼센트 변수를 문자열로 변환하여 저장하는 변수
+    public static String grade; // 퍼센트에 따른 등급 변수
 
     public ScoreScreen() {
-
         Color backGroundColor = new Color(86,187,241); // 배경색 설정
         Color buttonColor = new Color(77,119,255); // 버튼색 설정
 
-        setTitle("Score");
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle("Score"); // 프로그램 타이틀 "Score"
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // 창을 닫으면 창이 사라짐
 
-        Container container = getContentPane();
+        Container container = getContentPane(); // 프레임에 부착된 컨텐트팬 알아내기
+        container.setBackground(backGroundColor); // 컨텐트팬의 색 backGroundColor 설정
+        container.setLayout(null); // 컨텐트팬의 배치관리자 없음
 
-        container.setBackground(backGroundColor);
-
-        container.setLayout(null);
-
-        // 좌표값 알아내기
+        // 좌표값 알아내기////////////////////////////////////////
         JLabel la = new JLabel();
         container.addMouseListener(new MouseListener() {
             public void mouseClicked(MouseEvent e) {}
@@ -46,20 +43,26 @@ public class ScoreScreen extends JFrame {
             public void mouseEntered(MouseEvent e) {}
             public void mouseExited(MouseEvent e) {}
         });
+        /////////////////////////////////////////////////////////
 
-        JLabel scoreTitle = new JLabel("Score");
-        scoreTitle.setFont(new Font("Slab Serif",Font.ITALIC,80));
-        scoreTitle.setForeground(Color.WHITE);
-        scoreTitle.setLocation(35,35);
-        scoreTitle.setSize(300,65);
-        container.add(scoreTitle);
+        // ===[ Title ]=====================================================================
+        JLabel scoreTitle = new JLabel("Score"); // scroreTitle 라벨 문자열과 함께 생성
+        scoreTitle.setFont(new Font("Slab Serif",Font.ITALIC,80)); // 폰트 설정
+        scoreTitle.setForeground(Color.WHITE); // scoreTitle 라벨의 글씨 하얀색 설정
+        scoreTitle.setLocation(35,35); // scoreTitle 라벨의 위치
+        scoreTitle.setSize(300,65); // scoreTitle 라벨의 크기
+        container.add(scoreTitle); // 컨텐트팬에 scoreTitle 라벨 부착
 
-        JLabel timeLabel = new JLabel("Time");
-        timeLabel.setForeground(Color.WHITE);
+        // ===[ Time ]=====================================================================
+        JLabel timeLabel = new JLabel("Time"); // timeLabel 라벨 문자열과 함께 생성
+        MainScreen.settingItalicLabel(timeLabel,5,110,200,50,40);
+        /*
+        timeLabel.setForeground(Color.WHITE); // timeLabel 라벨의 글씨 하얀색 설정
         timeLabel.setFont(new Font("Slab Serif", Font.BOLD, 40));
         timeLabel.setLocation(5, 110);
         timeLabel.setSize(200, 50);
         container.add(timeLabel);
+         */
 
         JLabel timeValueLabel = new JLabel(String.valueOf(MainScreen.time));
         timeValueLabel.setForeground(Color.WHITE);
@@ -119,13 +122,37 @@ public class ScoreScreen extends JFrame {
         resultLabel.setLocation(70, 340);
         resultLabel.setSize(225, 50);
         container.add(resultLabel);
+        /**
+        if (percent >= 95) {
+            grade = "S+";
+        } else if (percent >= 90) {
+            grade = "S";
+        } else if (percent >= 85) {
+            grade = "A+";
+        } else if (percent >= 80) {
+            grade
+        }
+         **/
+        switch ((int) (percent/5)) {
+            case 20:
+            case 19: grade = "S+"; break;
+            case 18: grade = "S"; break;
+            case 17: grade = "A+"; break;
+            case 16: grade = "A"; break;
+            case 15: grade = "B+"; break;
+            case 14: grade = "B"; break;
+            case 13: grade = "C+"; break;
+            case 12: grade = "C"; break;
+            case 11: grade = "D+"; break;
+            default: grade = "F"; break;
+        }
 
-        JLabel grade = new JLabel("S+");
-        grade.setForeground(Color.YELLOW);
-        grade.setFont(new Font("Slab Serif",Font.BOLD, 200));
-        grade.setLocation(400,65);
-        grade.setSize(300,160);
-        container.add(grade);
+        JLabel gradeLabel = new JLabel(grade);
+        gradeLabel.setForeground(Color.YELLOW);
+        gradeLabel.setFont(new Font("Slab Serif",Font.BOLD, 200));
+        gradeLabel.setLocation(400,65);
+        gradeLabel.setSize(300,160);
+        container.add(gradeLabel);
 
         /** 포기 안한다. 현수 몰래 넣을 것.
         JLabel saying = new JLabel("There is no substitute for hard work. - Thomas Edison");
