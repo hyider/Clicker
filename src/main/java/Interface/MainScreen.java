15c92c68870f4372d0e63904d02bcb39585fdf0d
PTH <hyider2378@gmail.com>
2022-05-09 21:38:52 +0900

    수틀리면복구할거

--- src/main/java/Interface/MainScreen.java
+++ src/main/java/Interface/MainScreen.java
@@ -5,40 +5,30 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+// 프로그램 실행 시 가장 처음 나오는 화면
 public class MainScreen extends JFrame {
-    public static JLabel modeJLabel;
+    public static JLabel modeJLabel; // 게임 모드를 나타내는 라벨
 
-    public static int size;
-    public static int speed;
-    public static int frequency;
-    public static int time;
-
-    public static int hitCount;
+    public static int size; // 타겟의 사이즈 변수
+    public static int speed; // 타겟의 속도 변수
+    public static int frequency; // 타겟의 빈도 변수
+    public static int time; // 게임의 시간 변수
 
     public MainScreen() {
-        size = 5;
-        speed = 5;
-        frequency = 5;
-        time = 10;
-
-        setTitle("AimSimulator"); // 프로그램 타이틀 "AimSimulator"
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창을 닫으면 프로그램 종료
+        size = 5;   // 타겟 사이즈 변수, 초기값 5
+        speed = 5; // 타겟 속도 변수, 초기값 5
+        frequency = 5; // 타겟 빈도 변수, 초기값 5
+        time = 10; // 게임 시간 변수, 초기값 10
 
         Color backGroundColor = new Color(86,187,241); // 배경색 설정
         Color buttonColor = new Color(77,119,255); // 버튼색 설정
 
+        setTitle("AimSimulator"); // 프로그램 타이틀 "AimSimulator"
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창을 닫으면 프로그램 종료
+
         Container container = getContentPane(); // 프레임에 부탁된 컨텐트팬 알아내기
         container.setBackground(backGroundColor); // 컨탠트팬의 색 backGroundColor 설정
-        container.setLayout(null); // 배치관리자는 없음
-
-        /**
-        JButton test = new JButton("Test");
-        test.setSize(100,100);
-        test.setLocation(350,150);
-        test.paint(container,,);
-        container.add(test);
-        **/
+        container.setLayout(null); // 컨텐트팬의 배치관리자 없음
 
         // ====제목==========================================================================
         JLabel title = new JLabel("AimSimulator"); // title 라벨 문자열과 함께 생성
@@ -64,7 +54,6 @@ public class MainScreen extends JFrame {
         modeJLabel.setSize(200,50); // modeJLabel 라벨의 크기 설정
         container.add(modeJLabel); // 컨텐트팬에 modeJLabel 라벨 부착
 
-
         // ==[Start_버튼]=======================================================================
         JButton startButton = new JButton("Start"); // startButton 버튼 문자열과 함께 생성
         startButton.setFont(new Font("Slab Serif",Font.BOLD,50)); // 폰트 설정
@@ -74,6 +63,7 @@ public class MainScreen extends JFrame {
         startButton.setSize(200,100); // startButton 버튼의 크기
         container.add(startButton); // 컨텐트팬에 startButton 버튼 부착
 
+        // Start 버튼 클릭 시 StartScreen 실행, 모드를 설정 안하면 경고창 발생 후 확인 시 SettingScreen 실행
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -96,6 +86,7 @@ public class MainScreen extends JFrame {
         readMeButton.setSize(175,75); // readMeButton 버튼의 크기 설정
         container.add(readMeButton); // 컨텐트팬에 readMeButton 버튼 부착
 
+        // Read 버튼 클릭 시 ReadScreen 클래스 실행
         readMeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -112,6 +103,7 @@ public class MainScreen extends JFrame {
         settingButton.setSize(175,75); // settingButton 버튼의 크기 설정
         container.add(settingButton); // 컨텐트팬에 settingButton 버튼 부착
 
+        // Setting 버튼을 클릭 시 SettingScreen 클래스 실행
         settingButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -128,6 +120,7 @@ public class MainScreen extends JFrame {
         quitButton.setSize(175,75); // quitButton 버튼의 크기 설정
         container.add(quitButton); // 컨텐트팬에 quitButton 버튼 부착
 
+        // Quit 버튼을 클릭 시 시스템 종료
         quitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -136,14 +129,25 @@ public class MainScreen extends JFrame {
         });
 
         setSize(800,500); // 창의 크기 설정
-        // ***************************************************************************
         setResizable(false); // 창의 크기 고정
         setLocationRelativeTo(null); // 창이 가운데에 나타남
-        // ***************************************************************************
         setVisible(true); // 화면에 창을 나타냄
-
-
     }
+
+    /**
+    // ==[Setting_버튼]=======================================================================
+    JButton settingButton = new JButton("Setting"); // settingButton 버튼 문자열과 함께 생성
+        settingButton.setFont(new Font("Slab Serif",Font.BOLD,40)); // 폰트 설정
+        settingButton.setForeground(Color.WHITE); // settingButton 버튼의 글씨색 설정
+        settingButton.setBackground(buttonColor); // settingButton 버튼의 배경색 설정
+        settingButton.setLocation(575,250); // settingButton 버튼의 위치 설정
+        settingButton.setSize(175,75); // settingButton 버튼의 크기 설정
+        container.add(settingButton); // 컨텐트팬에 settingButton 버튼 부착
+
+    public void createButton(String buttonName, int locationX, int locationY, int sizeX, int sizeY) {
+        JButton buttonName = new JButton(buttonName);
+    }
+    **/
 
     public static void main(String[] args) {
         new MainScreen(); // 메인화면 호출
